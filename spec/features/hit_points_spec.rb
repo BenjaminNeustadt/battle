d80c73da38fe0b135dd6f0e7d1b30d81e8eb5597
benjamin <b.james.neustadt@gmail.com>
2022-06-01 19:34:05 +0200

    created helpers folderto reuse a file that was being used recurrently in the rspec

---
+++ spec/features/hit_points_spec.rb
@@ -0,0 +1,8 @@
+feature 'Hit points' do
+  scenario 'show player 2 hit points' do
+    visit('/')
+    sign_in_and_play
+    save_and_open_page 
+    expect(page).to have_content 'Slayer: 60HP'
+  end
+end
